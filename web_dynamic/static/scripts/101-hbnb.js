dc5f4d883622ef93fb701384b1a552b38f646a84
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-09-24 14:47:11 -0500

    Add: states and cities filter. Working on reviews toggle

---
+++ web_dynamic/static/scripts/101-hbnb.js
@@ -0,0 +1,104 @@
+$(document).ready(function () {
+  const url = 'http://0.0.0.0:5001/api/v1/';
+  $.get(url + 'status/', (data, status) => {
+    if (status === 'success' && data.status === 'OK') {
+      $('#api_status').addClass('available');
+    }
+  });
+
+  const amenitiesSelect = {};
+  const statesSelect = {};
+  const citiesSelect = {};
+  $('input:checkbox').change(function () {
+    if ($(this).parents('.amenities').length !== 0) {
+      check($(this), amenitiesSelect, '.amenities h4');
+    } else if ($(this).parents('.locations')) {
+      if ($(this).parents('ul').length === 2) {
+        check($(this), citiesSelect, '.locations h4');
+      } else if ($(this).parents('ul').length === 1) {
+        check($(this), statesSelect, '.locations h4');
+      }
+    }
+  });
+
+  const check = (checkbox, filterDict, selector) => {
+    if (checkbox.is(':checked')) {
+      filterDict[checkbox.parent().attr('data-id')] = checkbox
+        .parent()
+        .attr('data-name');
+    } else {
+      delete filterDict[checkbox.parent().attr('data-id')];
+    }
+    $(selector).text(Object.values(filterDict).join(', '));
+  };
+
+  const search = (jsonBody = {}) => {
+    $.ajax({
+      url: url + 'places_search/',
+      type: 'POST',
+      data: JSON.stringify(jsonBody),
+      contentType: 'application/json ',
+      success: (places) => {
+        $('section.places').empty();
+        for (const place of places) {
+          $('section.places').append(`\
+            <article id="${place.id}">\
+              <div class="title_box">\
+                <h2>${place.name}</h2>\
+                <div class="price_by_night">$${place.price_by_night}</div>\
+              </div>\
+              <div class="information">\
+                <div class="max_guest">
+                  ${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}\
+                </div>\
+                <div class="number_rooms">
+                  ${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}\
+                </div>\
+                <div class="number_bathrooms">
+                  ${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}\
+                </div>\
+              </div>\
+              <div class="description">${place.description}</div>\
+              <div class=reviews>
+                <h2>Reviews <span class="show-reviews">Show</span></h2>
+              </div>
+            </article>\
+          `);
+        }
+      }
+    });
+  };
+
+  $(document).on('click', 'span.show-reviews', function () {
+    const parentArticle = $(this).parents('article');
+    const placeID = parentArticle.attr('id');
+    $.ajax({
+      url: url + 'places/' + placeID + '/reviews/',
+      type: 'GET',
+      success: (reviews) => {
+        for (const review of reviews) {
+          // Agregar el review al artículo respectivo. Aquí añade a todos los artículos.
+          $('div.reviews').append(`\
+            <ul>\
+              <li>\
+                <h3>Date: ${review.created_at}</h3>\
+                <p>${review.text}</p>\
+              </li>\
+            </ul>\
+          `);
+        }
+      }
+    });
+  });
+
+  $('#search').click(() => {
+    const filters = {
+      states: Object.keys(statesSelect),
+      cities: Object.keys(citiesSelect),
+      amenities: Object.keys(amenitiesSelect)
+    };
+    search(filters);
+  });
+
+  search();
+});
